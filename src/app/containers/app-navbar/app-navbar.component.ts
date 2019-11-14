4409867c03360c91da21d224b052525ff5849dc6
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2019-11-14 18:33:12 -0300

    Formatt Appnav

--- src/app/containers/app-navbar/app-navbar.component.ts
+++ src/app/containers/app-navbar/app-navbar.component.ts
@@ -15,39 +15,8 @@ import { AppApi } from '@api/app.api';
 @Component({
   selector: 'app-navbar',
   encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./app-navbar.scss'],
-  template: `
-    <nav class="row navbar navbar-default navbar-fixed-top">
-      <div class="navbar-container">
-        <div class="navbar__content">
-        <h3 *ngIf="header" class="navbar__header navbar-text">
-            <button *ngIf="mainIcon" class="navbar-btn__main btn-transparent"
-              (click)="handleMainIconClick()">
-              <icon [name]="mainIcon"></icon>
-            </button>
-            <icon [name]="headerIcon" *ngIf="headerIcon"></icon> {{ header }}
-          </h3>
-          <ng-content></ng-content>
-        </div>
-        <section class="navbar-text navbar-actions">
-          <app-navbar-user
-            [signedIn]="isSignIn()"
-            [userImageUrl]="(user$ | async).profile.imageUrl"
-            (signIn)="signInUser()"
-            ></app-navbar-user>
-          <app-navbar-menu
-            [appVersion]="appVersion$ | async"
-            [theme]="themes$ | async"
-            (themeChange)="changeTheme($event)"
-            [signedIn]="isSignIn()"
-            (signOut)="signOutUser()"
-            (versionUpdate)="updateVersion()"
-            (versionCheck)="checkVersion()"
-          ></app-navbar-menu>
-        </section>
-      </div>
-    </nav>
-  `,
+  styleUrls: ['./app-navbar.component.scss'],
+  template: `./app-navbar.component.html`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppNavbarComponent implements OnInit {
