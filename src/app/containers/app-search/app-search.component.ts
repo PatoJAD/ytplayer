9b4f6f41cdc11e8d9bddc25baec034da762c2cb8
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2019-11-14 18:48:38 -0300

    App search fix format

--- src/app/containers/app-search/app-search.component.ts
+++ src/app/containers/app-search/app-search.component.ts
@@ -8,32 +8,8 @@ import * as fromPlayerSearch from '@core/store/player-search';
 
 @Component({
   selector: 'app-search',
-  styleUrls: ['./app-search.scss'],
-  template: `
-  <article
-    infiniteScroll
-    [infiniteScrollDistance]="2"
-    [infiniteScrollDisabled]="currentPlaylist$ | async"
-    (scrolled)="searchMore()"
-    [immediateCheck]="true">
-    <app-navbar>
-      <div class="navbar-header">
-        <player-search
-          [query]="query$ | async"
-          (queryChange)="resetPageToken($event)"
-          (search)="search($event)"
-        ></player-search>
-      </div>
-      <button-group class="nav-toolbar"
-        [buttons]="presets$ | async"
-        [selectedButton]="queryParamPreset$ | async"
-        (buttonClick)="updatePreset($event)"
-      ></button-group>
-      <search-navigator></search-navigator>
-    </app-navbar>
-    <router-outlet></router-outlet>
-    </article>
-    `
+  styleUrls: ['./app-search.component.scss'],
+  template: './app-search.component.scss'
 })
 export class AppSearchComponent implements OnInit {
   query$ = this.store.pipe(select(fromPlayerSearch.getQuery));
