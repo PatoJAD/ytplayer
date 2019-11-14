9624b754bf990d6ca76fdaeef2abb20551f09f78
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2019-11-14 18:43:01 -0300

    pl-view component format

--- src/app/containers/playlist-view/playlist-view.component.ts
+++ src/app/containers/playlist-view/playlist-view.component.ts
@@ -5,26 +5,7 @@ import { PlaylistProxy } from './playlist-view.proxy';
 @Component({
   selector: 'playlist-view',
   styleUrls: ['./playlist-view.component.scss'],
-  template: `
-  <article>
-    <app-navbar [header]="header$ | async"
-      [mainIcon]="'chevron-left'"
-      (headerMainIconClick)="handleBack()">
-    </app-navbar>
-    <div class="row">
-      <playlist-viewer class="clearfix"
-        [videos]="videos$ | async"
-        [playlist]="playlist$ | async"
-        [queuedPlaylist]="nowPlaylistIds$ | async"
-        (playPlaylist)="playPlaylist($event)"
-        (queuePlaylist)="queuePlaylist($event)"
-        (playVideo)="playVideo($event)"
-        (queueVideo)="queueVideo($event)"
-        (unqueueVideo)="unqueueVideo($event)"
-      ></playlist-viewer>
-    </div>
-  </article>
-  `
+  template: './playlist-view.component.html'
 })
 export class PlaylistViewComponent implements OnInit {
   playlist$ = this.playlistProxy.fetchPlaylist(this.route);
